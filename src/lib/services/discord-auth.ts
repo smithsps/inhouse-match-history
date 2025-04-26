5a2bc73daa6a23291a4a0d24f820ee48310fb4ef
Simon Smith <simon@smnth.dev>
2025-04-25 21:32:08 -0400

    Change web domain to inhouses

--- src/lib/services/discord-auth.ts
+++ src/lib/services/discord-auth.ts
@@ -5,5 +5,5 @@ import { dev } from "$app/environment";
 export const discord = new Discord(
     env.DISCORD_CLIENT_ID,
     env.DISCORD_CLIENT_SECRET,
-    dev ? "http://localhost:5173/login/discord/callback" : "https://inhouse.smnth.net/login/discord/callback",
+    dev ? "http://localhost:5173/login/discord/callback" : "https://inhouses.smnth.net/login/discord/callback",
 );
